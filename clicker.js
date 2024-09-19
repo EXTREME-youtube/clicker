4f7908e6472f1df27d91da97fdf9fd633ce48c11
EXTREME-youtube <sterlinghodgeshi@gmail.com>
2024-09-19 01:57:32 -0500

    Add files via upload

---
+++ clicker.js
@@ -0,0 +1,20 @@
+function initClickerGame() {
+    const counterEl = document.getElementById('counter');
+    const luckyBtn = document.getElementById('luckyBtn');
+    const messageEl = document.getElementById('message');
+    let isCooldown = false;
+
+    luckyBtn.addEventListener('click', () => {
+        // Rest of your logic...
+
+    });
+
+    // Rest of your code...
+}
+
+window.onload = initClickerGame;
+luckyBtn.addEventListener('click', function() {
+    // Inside this function, "this" refers to the luckyBtn element
+    // Rest of your logic...
+
+    });
